ba47308a2e7c19eb008d32e9968de54fa7a50dd2
NiltonRicardoP <rcrd.pimentel@live.com>
2022-03-02 02:33:25 -0300

    Projeto Laravel finalizado

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -79,9 +79,24 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if($user) {
+
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach($userEvents as $userEvent) {
+                if($userEvent['id'] == $id) {
+                    $hasUserJoined = true;
+                }
+            }
+
+        }
+
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show',['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show',['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
 
     }
 
@@ -90,7 +105,11 @@ class EventController extends Controller
 
         $events = $user->events;
 
-        return view('events.dashboard', ['events' => $events]);
+        $eventsAsParticipant = $user->eventsasparticipant;
+
+        return view('events.dashboard', 
+        ['events' => $events, 'eventsasparticipant' => $eventsAsParticipant]
+    );
     }
 
     public function destroy($id) {
@@ -99,5 +118,71 @@ class EventController extends Controller
 
         return redirect('/dashboard')->with('msg', 'Evento excluído com sucesso!');
     }
+
+    public function edit($id) {
+
+        $user = auth()->user();
+
+        $event = Event::findOrFail($id);
+
+        if($user->id != $event->user_id) {
+            return redirect('/dashboard');
+        }
+
+        return view('events.edit', ['event' => $event]);
+    }
+
+
+    public function update(Request $request) {
+
+        $data = $request->all();
+
+        // Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $data['image'] = $imageName;
+
+        }
+
+        Event::findOrFail($request->id)->update($data);
+
+        return redirect('/dashboard')->with('msg', 'Evento editado com sucesso!');
+
+    }
+
+    public function joinEvent($id) {
+        
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->attach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', "Sua presença foi confirmada no evento " . $event->tittle);
+
+
+    }
+
+    public function leaveEvent($id) {
+
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->detach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', "Você saiu com sucesso do evento: " . $event->tittle);
+        
+
+    }
+
 
 }
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -12,9 +12,22 @@
             <div id="info-container" class="col-md-6">
                 <h1>{{ $event->tittle }}</h1>
                 <p class="event-city"><ion-icon name="location-outline"></ion-icon>{{ $event->city }}</p>
-                <p class="evets-participants"><ion-icon name="people-outline"></ion-icon> X Participantes</p>
+                <p class="evets-participants"><ion-icon name="people-outline"></ion-icon> {{ count($event->users) }}</p>
                 <p class="event-owner"><ion-icon name="star-outline"></ion-icon> {{ $eventOwner['name'] }} </p>
-                <a href="#" class="btn btn-primary" id="event-submit">Confirmar Presença</a>
+                <form action="/events/join/{{ $event->id }}" method="POST">
+                    @if(!$hasUserJoined)
+                        @csrf
+                        <a href="/events/join/{{ $event->id }}" 
+                        class="btn btn-primary" 
+                        id="event-submit"
+                        onclick="event.preventDefault();
+                        this.closest('form').submit();">
+                        Confirmar Presença
+                    @else
+                          <p class="already-joined-msg">Você já está participando deste evento: {{ $event->tittle }}</p>  
+                    @endif
+                </a>
+                </form>
                 <h3>O Evento conta com: </h3>
                 <ul id="items-list">
                     @foreach ($event->items as $item )
